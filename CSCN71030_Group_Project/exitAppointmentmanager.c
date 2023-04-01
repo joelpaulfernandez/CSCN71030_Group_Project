5b8d7c8ce4f1fc91c4f3201a37233652281c4420
brianegona <113472988+brianegona@users.noreply.github.com>
2023-04-01 16:55:45 -0400

    Update exitAppointmentmanager.c

---
+++ CSCN71030_Group_Project/exitAppointmentmanager.c
@@ -0,0 +1,9 @@
+#include "exitAppointmentmanager.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void exitAppointmentmanager() {
+	printf("Thank you very much for using this appoinment manager!\n");
+	exit(0);	//REQ-SYS-010
+}
